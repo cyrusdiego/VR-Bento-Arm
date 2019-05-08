114938e881b4a223aded566637bc3da90b2b27f4
Cyrus Diego <cdiego@ualberta.ca>
2019-05-08 12:08:54 -0600

    basic idea for camera movement is good for forward

--- Notes.md
+++ Notes.md
@@ -12,3 +12,9 @@
 - Fixed constraint bug for shoulder 
     - i think this should still be improved (not the best solution to the constraint)
 - Got basic rotation for the elbow 
+- Got Wrist rotation
+**Rotations** 
+    - Toggle Pivot 
+    - Create/move empty game object to pivot location
+    - nest object to rotate about that pivot 
+    - apply rotaton to empty game object 
---
+++ VR-Bento-Arm/Assets/Scripts/CameraMovementScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementScript : MonoBehaviour
+{
+    public Transform cameraTransform = null;
+    private int cameraSpeed = 100;
+    private float wKeyPressTime;
+    private float minTime = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform.position = new Vector3(-185, 125, -605); // arbitrary, this vector gives side profile of arm 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            wKeyPressTime = Time.timeSinceLevelLoad;
+   
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            if(Time.timeSinceLevelLoad - wKeyPressTime > minTime)
+            {
+                cameraTransform.Translate(new Vector3(0, 0, cameraSpeed * Time.deltaTime));
+
+            }
+        }
+    }
+}
