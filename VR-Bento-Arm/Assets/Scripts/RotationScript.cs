a76d6f9232655cbf442517b6ded2bb048085d438
Cyrus Diego <cdiego@ualberta.ca>
2019-05-09 16:28:52 -0600

    got correct pivot points, double check

--- VR-Bento-Arm/Assets/Scripts/RotationScript.cs
+++ VR-Bento-Arm/Assets/Scripts/RotationScript.cs
@@ -7,14 +7,17 @@ public class RotationScript : MonoBehaviour
     public float robotShoulderSliderValue = 0.0f;
     public float robotElbowSliderValue = 0.0f;
     public float robotWristSliderValue = 0.0f;
+    public float robotTwistSliderValue = 0.0f;
 
     public Transform Shoulder = null;
     public Transform Elbow = null;
     public Transform Wrist = null;
+    public Transform Twist = null;
 
     public float shoulderTurnRate = -0.2f;
     public float elbowTurnRate = -0.2f;
     public float wristTurnRate = -0.2f;
+    public float twistTurnRate = 0.2f;
 
     public GameObject[] shells = new GameObject[3];
     // Start is called before the first frame update
@@ -47,13 +50,15 @@ public class RotationScript : MonoBehaviour
 
         Wrist.Rotate(0, 0, Mathf.Floor(robotWristSliderValue * wristTurnRate), Space.Self);
 
+        Twist.Rotate(Mathf.Floor(robotTwistSliderValue * twistTurnRate), 0, 0, Space.Self);
+
         if (Input.GetMouseButtonUp(0)) // the "0" is refering to a button mapping
         {
             //resets the sliders back to 0 when you lift up on the mouse click down.
             robotShoulderSliderValue = 0;
             robotElbowSliderValue = 0;
             robotWristSliderValue = 0;
-     
+            robotTwistSliderValue = 0;
         }
     }
     private void OnGUI()
@@ -62,9 +67,10 @@ public class RotationScript : MonoBehaviour
         robotShoulderSliderValue = GUI.HorizontalSlider(new Rect(25, 80, 100, 30), robotShoulderSliderValue, -10.0f, 10.0f);
         robotElbowSliderValue = GUI.HorizontalSlider(new Rect(25, 120, 100, 30), robotElbowSliderValue, -10.0f, 10.0f);
         robotWristSliderValue = GUI.HorizontalSlider(new Rect(25, 160, 100, 30), robotWristSliderValue, -10.0f, 10.0f);
-       
+        robotTwistSliderValue = GUI.HorizontalSlider(new Rect(25, 200, 100, 30), robotTwistSliderValue, -10.0f, 10.0f);
+
         // shows/hides arm shells in game 
-        if(GUI.Button(new Rect(10, 10, 50, 50), "Shells"))
+        if (GUI.Button(new Rect(10, 10, 50, 50), "Shells"))
         {
             foreach(GameObject shell in shells)
             {
