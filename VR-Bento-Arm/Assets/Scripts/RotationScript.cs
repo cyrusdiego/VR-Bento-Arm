42fcd816f4cdc3670c8f451752711a4c61ce8203
Cyrus Diego <cdiego@ualberta.ca>
2019-05-10 11:01:52 -0600

    made it a bit cleaner for GUI and code

--- VR-Bento-Arm/Assets/Scripts/RotationScript.cs
+++ VR-Bento-Arm/Assets/Scripts/RotationScript.cs
@@ -4,79 +4,71 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    public float robotShoulderSliderValue = 0.0f;
-    public float robotElbowSliderValue = 0.0f;
-    public float robotWristSliderValue = 0.0f;
-    public float robotTwistSliderValue = 0.0f;
-
-    public Transform Shoulder = null;
-    public Transform Elbow = null;
-    public Transform Wrist = null;
-    public Transform Twist = null;
-
-    public float shoulderTurnRate = -0.2f;
-    public float elbowTurnRate = -0.2f;
-    public float wristTurnRate = -0.2f;
-    public float twistTurnRate = 0.2f;
-
+    private Dictionary<string, Transform> robotTransforms = new Dictionary<string, Transform>();
     public GameObject[] shells = new GameObject[3];
+    public Transform[] Transforms = new Transform[5];
+    private string[] transformNames = { "Shoulder", "Elbow", "Forearm Rotation", "Wrist Flexion", "Open Hand" };
+    public float turnRate = 0.2f;
+    private float sliderValue = 0.0f;
+    private string mode;
+    private int modeitr = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        mode = transformNames[modeitr++];
+        int i = 0;
+        foreach (Transform transform in Transforms)
+        {
+            robotTransforms.Add(transformNames[i], transform);
+            i++;
+        }
     }
-
     // Update is called once per frame
     void Update()
     {
- 
-        if (!(Shoulder.eulerAngles.y > 90 && Shoulder.eulerAngles.y < 270))
-        {
-            Shoulder.Rotate(0, Mathf.Floor(robotShoulderSliderValue * shoulderTurnRate), 0, Space.Self);
-
-        }
-        else if (Shoulder.eulerAngles.y >= 90 && Shoulder.eulerAngles.y <= 180)
-        {
-            Shoulder.eulerAngles = new Vector3(0, 90, 0);  
-
-        }
-        else if (Shoulder.eulerAngles.y <= 270 && Shoulder.eulerAngles.y > 180)
-        {
-            Shoulder.eulerAngles = new Vector3(0, 270, 0);
-        }
         
-
-        Elbow.Rotate(0, 0, Mathf.Floor(robotElbowSliderValue * elbowTurnRate), Space.Self);
-
-        Wrist.Rotate(0, 0, Mathf.Floor(robotWristSliderValue * wristTurnRate), Space.Self);
-
-        Twist.Rotate(Mathf.Floor(robotTwistSliderValue * twistTurnRate), 0, 0, Space.Self);
-
-        if (Input.GetMouseButtonUp(0)) // the "0" is refering to a button mapping
+        switch(modeitr % 5)
         {
-            //resets the sliders back to 0 when you lift up on the mouse click down.
-            robotShoulderSliderValue = 0;
-            robotElbowSliderValue = 0;
-            robotWristSliderValue = 0;
-            robotTwistSliderValue = 0;
+            case 0:
+                robotTransforms[mode].Rotate(0, Mathf.Floor(sliderValue * turnRate), 0, Space.Self); 
+                break;
+            case 1:
+                robotTransforms[mode].Rotate(0, Mathf.Floor(sliderValue * turnRate), 0, Space.Self);
+                break;
+            case 2:
+                robotTransforms[mode].Rotate(0, 0, Mathf.Floor(sliderValue * turnRate), Space.Self);
+                break;
+            case 3:
+                robotTransforms[mode].Rotate(Mathf.Floor(sliderValue * turnRate), 0, 0, Space.Self);
+                break;
+            case 4:
+                robotTransforms[mode].Rotate(0, 0, Mathf.Floor(sliderValue * turnRate), Space.Self);
+                break;
+        
         }
     }
-    private void OnGUI()
-    {
-        //creates the slider and sets it 25 pixels in x, 80 in y, 100 wide and 30 tall.
-        robotShoulderSliderValue = GUI.HorizontalSlider(new Rect(25, 80, 100, 30), robotShoulderSliderValue, -10.0f, 10.0f);
-        robotElbowSliderValue = GUI.HorizontalSlider(new Rect(25, 120, 100, 30), robotElbowSliderValue, -10.0f, 10.0f);
-        robotWristSliderValue = GUI.HorizontalSlider(new Rect(25, 160, 100, 30), robotWristSliderValue, -10.0f, 10.0f);
-        robotTwistSliderValue = GUI.HorizontalSlider(new Rect(25, 200, 100, 30), robotTwistSliderValue, -10.0f, 10.0f);
 
-        // shows/hides arm shells in game 
-        if (GUI.Button(new Rect(10, 10, 50, 50), "Shells"))
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 25, 100, 20), "Shells"))
         {
-            foreach(GameObject shell in shells)
+            foreach (GameObject shell in shells)
             {
                 shell.SetActive(!shell.activeSelf);
             }
         }
 
+        if (GUI.Button(new Rect(10, 50, 150, 20), mode))
+        {
+            mode = transformNames[modeitr++ % 5];
+        }
+
+        sliderValue = GUI.HorizontalSlider(new Rect(10, 75, 100, 30), sliderValue, -10.0f, 10.0f);
+
+        if (Input.GetMouseButtonUp(0)) // the "0" is refering to a button mapping
+        {
+            sliderValue = 0;
+        }
     }
 }
