575d721d13c849c20982c8cf5a0dd7a025f2b0c4
Cyrus Diego <cdiego@ualberta.ca>
2019-05-17 10:49:02 -0600

    added comments

--- VR-Bento-Arm/Assets/Scripts/RotationScript.cs
+++ VR-Bento-Arm/Assets/Scripts/RotationScript.cs
@@ -2,7 +2,7 @@
  * RotationScript.cs 
  * Created by: Cyrus Diego May 7, 2019 
  * 
- * Controls rotation of the virtual bento arm using a slider and button GUI on screen
+ * Controls rotation of the virtual bento arm using the keyboard 
  */
 
 using System;
@@ -12,13 +12,21 @@ using UnityEngine;
 
 public class RotationScript : MonoBehaviour
 {
-    private Dictionary<string, Rigidbody> robotRigidBody = new Dictionary<string, Rigidbody>();
-    private Dictionary<string, Tuple<float, float>> torque_velocityVals = new Dictionary<string, Tuple<float, float>>();
+    // Name : rigidbody and Name : torque and velocity storage 
+    private Dictionary<string, Rigidbody> robotRigidBody = 
+            new Dictionary<string, Rigidbody>();
 
-    private string[] rigidBodyNames = { "Shoulder", "Elbow", "Forearm Rotation", "Wrist Flexion", "Open Hand" };
+    private Dictionary<string, Tuple<float, float>> torque_velocityVals = 
+            new Dictionary<string, Tuple<float, float>>();
+
+    // Rotation modes and motor properties 
+    private string[] rigidBodyNames = { "Shoulder", "Elbow", "Forearm Rotation", 
+            "Wrist Flexion", "Open Hand" };
+
     public float[] torqueVals = { 1.319f, 2.436f, 0.733f, 0.611f, 0.977f };
     public float[] velocityVals = { 6.27f, 5.13f, 737f, 9.90f, 9.90f }; // rpm 
 
+    // Drag and Drop in Inspector 
     public GameObject[] shells = new GameObject[6];
     public Rigidbody[] rigidBodies = new Rigidbody[5];
 
@@ -26,54 +34,71 @@ public class RotationScript : MonoBehaviour
     private string mode;
     private int modeitr = 0;
 
+    // sets properties for the configurable joints 
     private ConfigurableJoint joint = null;
     private JointDrive motor;
     private SoftJointLimit tempAngle1,tempAngle2;
 
+    // Used for Angular Limits 
     private float deltaAngle;
     public float[] angleLimits = new float[5];
 
+    // Checks if arm has collided with itself or another collider / rigid body 
     private bool collided = false;
-    // Start is called before the first frame update
+
+
+    /*
+        @brief: Called before first frame. Initializes and fills
+        data structures and properties 
+    */
     void Start() {
-        mode = rigidBodyNames[modeitr++];  // sets the first mode 
+        // sets the first mode
+        mode = rigidBodyNames[modeitr++];  
 
         // convert to rad / s
-        for(int j = 0; j < 5; j++) {
+        for(int j = 0; j < velocityVals.Length; j++) {
             velocityVals[j] *= (Mathf.PI / 30);
         }
 
+        // adds to the dictionaries for easy lookup 
         int i = 0;
-        foreach (Rigidbody rigidbody in rigidBodies) {// adds to the dictionaries for easy lookup 
+        foreach (Rigidbody rigidbody in rigidBodies) {
             robotRigidBody.Add(rigidBodyNames[i], rigidbody);  
-            torque_velocityVals.Add(rigidBodyNames[i], new Tuple<float, float>(torqueVals[i], velocityVals[i]));
+            torque_velocityVals.Add(rigidBodyNames[i], 
+                    new Tuple<float, float>(torqueVals[i], velocityVals[i]));
             i++;
         }
+
+        // Sets initial settings 
         setKinematic();
         setJointMotor();
         setRotationAxis();
     }
 
-    public void collisionDetection(bool msg){
-        Debug.Log("yo");
+    /*
+        @brief: called by bounding box gameObjects and will set collided to TRUE
+        when the box colliders collide with one another 
+
+        @param: TRUE if box collider collides with another box collider 
+    */
+    public void collisionDetection(bool msg) {
         collided = msg;
     }
 
+    /*
+        @brief: changes the joint being rotated 
+    */
     private void setJointMotor() {
         joint = robotRigidBody[mode].gameObject.GetComponent<ConfigurableJoint>();
-        joint.connectedAnchor = new Vector3(-101.2f, 125.1f, 0);
-
     }
 
-    // stops the movement of all the joints in between modes 
-    private void setAngularVelocity() {
-        foreach (string name in rigidBodyNames) {
-            robotRigidBody[name].angularVelocity = Vector3.zero;
-        }
+    /*
+        @brief: sets the "isKinematic" property to false for the current joint
+        and sets it false for the other joints 
 
-    }
-
-    // Ensures the current joint being rotated is not a kinematic object and the rest are 
+        If isKinematic is enabled, Forces, collisions or joints 
+        will not affect the rigidbody anymore (Unity API)
+    */
     private void setKinematic() {
         for(int i = modeitr % 5; i < 5; i++) {
             robotRigidBody[rigidBodyNames[i]].isKinematic = true;
@@ -81,8 +106,11 @@ public class RotationScript : MonoBehaviour
         robotRigidBody[mode].isKinematic = false;
     }
 
-    // Update is called once per frame
+    /*
+        @brief: called once per frame
+    */
     void FixedUpdate() {
+        // Checks if the arm has collided with a box collider 
         if(!collided){
             rotateArm();
         } else {
@@ -90,6 +118,10 @@ public class RotationScript : MonoBehaviour
         }
     }
 
+    /*
+        @brief: similar to rotateArm(), will only allow the arm to rotate in 
+        one direction when it has collided with another box collider 
+    */
     private void restrictRotation() {
         if(currentSliderValue > 0) {
             if (sliderValue >= 0) {
@@ -99,7 +131,8 @@ public class RotationScript : MonoBehaviour
                 joint.targetRotation = Quaternion.Euler(new Vector3(0, 0, 0));
 
             } else if(sliderValue < 0) {
-                joint.targetAngularVelocity = new Vector3(torque_velocityVals[mode].Item2 * -1, 0, 0);
+                joint.targetAngularVelocity = 
+                        new Vector3(torque_velocityVals[mode].Item2 * -1, 0, 0);
                 motor.maximumForce = torque_velocityVals[mode].Item1;
                 motor.positionSpring = 1.0f;
                 motor.positionDamper = 1000;
@@ -114,7 +147,8 @@ public class RotationScript : MonoBehaviour
                 joint.targetRotation = Quaternion.Euler(new Vector3(0, 0, 0));
 
             } else if(sliderValue > 0) {
-                joint.targetAngularVelocity = new Vector3(torque_velocityVals[mode].Item2, 0, 0);
+                joint.targetAngularVelocity =
+                         new Vector3(torque_velocityVals[mode].Item2, 0, 0);
                 motor.maximumForce = torque_velocityVals[mode].Item1;
                 motor.positionSpring = 1.0f;
                 motor.positionDamper = 1000;
@@ -124,8 +158,12 @@ public class RotationScript : MonoBehaviour
         }
     }
 
+    /*
+        @brief: rotates the joint with specified torque and maximum velocity 
+    */
     private void rotateArm() {
-                    currentSliderValue = sliderValue;
+        // stores current value, used in restrictRotation()
+        currentSliderValue = sliderValue;
 
         if (sliderValue == 0){
             joint.targetAngularVelocity = Vector3.zero;
@@ -134,14 +172,16 @@ public class RotationScript : MonoBehaviour
             joint.targetRotation = Quaternion.Euler(new Vector3(0, 0, 0));
 
         } else if(sliderValue > 0) {
-            joint.targetAngularVelocity = new Vector3(torque_velocityVals[mode].Item2, 0, 0);
+            joint.targetAngularVelocity = 
+                    new Vector3(torque_velocityVals[mode].Item2, 0, 0);
             motor.maximumForce = torque_velocityVals[mode].Item1;
             motor.positionSpring = 1.0f;
             motor.positionDamper = 1000;
             joint.angularXDrive = motor;
 
         } else {
-            joint.targetAngularVelocity = new Vector3(torque_velocityVals[mode].Item2 * -1, 0, 0);
+            joint.targetAngularVelocity = 
+                    new Vector3(torque_velocityVals[mode].Item2 * -1, 0, 0);
             motor.maximumForce = torque_velocityVals[mode].Item1;
             motor.positionSpring = 1.0f;
             motor.positionDamper = 1000;
@@ -149,6 +189,9 @@ public class RotationScript : MonoBehaviour
         }
     }
 
+    /*
+        @brief: sets the rotational axis depending on the joint
+    */
     private void setRotationAxis() {
         switch (modeitr % 5) {
             case 0: // open hand 
@@ -186,6 +229,7 @@ public class RotationScript : MonoBehaviour
                 break;
         }
 
+        // Adjusts the angular limit with the current euler angle 
         deltaAngle = (deltaAngle > 180) ? Mathf.FloorToInt(deltaAngle - 360) : Mathf.FloorToInt(deltaAngle);  // https://answers.unity.com/questions/554743/how-to-calculate-transformlocaleuleranglesx-as-neg.html
         tempAngle1.limit = -angleLimits[modeitr % 5] - deltaAngle;
         joint.lowAngularXLimit = tempAngle1;
@@ -193,7 +237,11 @@ public class RotationScript : MonoBehaviour
         joint.highAngularXLimit = tempAngle2;
     }
 
-    // Creates buttons and slider 
+
+    /*
+        @brief: draws the buttons and slider to change modes, hides extras, 
+        and rotates joints 
+    */
     void OnGUI() {
         // Hides arm-shells, 8020 stand, Table, and Desktop-Workspace
         if (GUI.Button(new Rect(10, 25, 100, 20), "Hide")) {
@@ -209,7 +257,6 @@ public class RotationScript : MonoBehaviour
         // the "0" is refering to a button mapping
         if (Input.GetMouseButtonUp(0)) { 
             sliderValue = 0;
-
         }
 
         // button to alternate between joints / freedom of movements 
@@ -217,7 +264,6 @@ public class RotationScript : MonoBehaviour
             mode = rigidBodyNames[modeitr++ % 5];  // changes the mode 
             setJointMotor();
             setKinematic();  // cycles through the rigid - bodies to set "isKinematic" property 
-            setAngularVelocity();  // resets the angular velocity of all the joints to ensure it stops moving 
             setRotationAxis();
         }
     }
