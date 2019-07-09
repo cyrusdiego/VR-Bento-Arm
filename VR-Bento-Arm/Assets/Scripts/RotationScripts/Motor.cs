e728a680fdac23506edcb70d74e0ef9b723299e3
Cyrus Diego <cdiego@ualberta.ca>
2019-07-09 08:57:45 -0600

    Replacing each rotation script with Motor.cs

---
+++ VR-Bento-Arm/Assets/Scripts/RotationScripts/Motor.cs
@@ -0,0 +1,93 @@
+/* 
+    BLINC LAB VIPER Project 
+
+ */
+using UnityEngine;
+using System;
+public class Motor : RotationBase
+{
+    // Inspector values
+    public BentoControl bentoControl = null;
+    public string rotationAxis = "";
+    public int arrayIndex = 255;
+    public float mass = 0;
+
+    private float direction;
+    private float velocity;
+
+    void Start()
+    {
+        axis = ((Axis)Enum.Parse(typeof(Axis),rotationAxis));
+
+        cj = gameObject.GetComponent<ConfigurableJoint>();
+        rb = gameObject.GetComponent<Rigidbody>();
+        go = gameObject;
+
+        configureCJ();
+        configureRB();
+    }
+
+    void FixedUpdate()
+    {
+        float direction = bentoControl.rotationArray[arrayIndex].Item1;
+        float velocity = bentoControl.rotationArray[arrayIndex].Item2;
+        
+        switch(direction)
+        {
+            case 0:
+                getAxis(0,velocity);
+                break;
+
+            case 1:
+                getAxis(-1,velocity);
+                break;
+
+            case 2:
+                getAxis(1,velocity);
+                break;
+        }
+    }
+
+    private void configureCJ()
+    {
+        cj.xMotion = ConfigurableJointMotion.Locked;
+        cj.yMotion = ConfigurableJointMotion.Locked;
+        cj.zMotion = ConfigurableJointMotion.Locked;
+
+        cj.angularXMotion = ConfigurableJointMotion.Free;
+        cj.angularYMotion = ConfigurableJointMotion.Locked;
+        cj.angularZMotion = ConfigurableJointMotion.Locked;
+
+        cj.autoConfigureConnectedAnchor = true;
+        switch(axis)
+        {
+            case Axis.x:
+                cj.axis = new Vector3(-1,0,0);
+            break;
+
+            case Axis.y:
+                cj.axis = new Vector3(0,-1,0);
+            break;
+
+            case Axis.z:
+                cj.axis = new Vector3(0,0,-1);
+            break;
+        }
+
+        cj.anchor = Vector3.zero;
+        cj.projectionMode = JointProjectionMode.PositionAndRotation;
+        cj.projectionAngle = 0.1f;
+        cj.rotationDriveMode = RotationDriveMode.XYAndZ;
+    }
+
+    private void configureRB()
+    {
+        rb.mass = mass;
+        rb.drag = 0;
+        rb.angularDrag = 0;
+        rb.useGravity = false;
+        rb.isKinematic = false;
+        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+    }
+}
+
