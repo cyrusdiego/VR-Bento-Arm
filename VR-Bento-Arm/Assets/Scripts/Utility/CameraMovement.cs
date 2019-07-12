6cf017c770bb72d8b8d2dcea6a0a0151a2abd70b
Cyrus Diego <cdiego@ualberta.ca>
2019-07-12 11:45:01 -0600

    camera movement toggles between keyboard and vr controllers

--- VR-Bento-Arm/Assets/Scripts/Utility/CameraMovement.cs
+++ VR-Bento-Arm/Assets/Scripts/Utility/CameraMovement.cs
@@ -10,47 +10,39 @@ public class CameraMovement : MonoBehaviour
     private SteamVR_Input_Sources Right;
     private SteamVR_Action action;
     private SteamVR_Action_Vector2 trackpad = null;
+    private SteamVR_Action_Boolean boolean;
+    private SteamVR_Action push;
     private int camZMovement = 0, camXMovement = 0, camYMovement = 0;
+    public BentoControl bentoControl = null;
     
     void Start()
     {
-        string path = "/actions/default/in/Trackpad";
-
         Left = SteamVR_Input_Sources.LeftHand;
         Right = SteamVR_Input_Sources.RightHand;
         
-        action = SteamVR_Action.FindExistingActionForPartialPath(path);
-        trackpad = (SteamVR_Action_Vector2)action;
+        action = SteamVR_Action.FindExistingActionForPartialPath("/actions/default/in/Trackpad");
+        push = SteamVR_Action.FindExistingActionForPartialPath("/actions/default/in/Button");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        checkJoystick();
+        trackpad = (SteamVR_Action_Vector2)action;
+        boolean = (SteamVR_Action_Boolean)push;
+
+        if(bentoControl.controlToggle)
+        {
+            print("in here bitch");
+            checkJoystick();
+        }
+        else
+        {
+            checkKeyboard();
+        }
         updateX();
         updateY();
         updateZ();
-    }
-
-    /*
-        @brief: checks if a button is being held / squeezed
-
-        @param: the button being squeezed 
-    */
-    private int checkHold(string button) 
-    {
-        if(Input.GetAxis(button) > 0.8)
-        {
-            return 1;
-        } 
-        else if(Input.GetAxis(button) < -0.8)
-        {
-            return -1;
-        } 
-        else
-        {
-            return 0;
-        }
     }
 
     private int checkHold(float val)
@@ -69,14 +61,55 @@ public class CameraMovement : MonoBehaviour
         }
     }
 
+    private int checkHold(KeyCode left, KeyCode right)
+    {
+        if((Input.GetKey(left) && Input.GetKey(right)) || (!Input.GetKey(left) && !Input.GetKey(right)))
+        {
+            return 0;
+        }
+        else if(Input.GetKey(left) && !Input.GetKey(right))
+        {
+            return -1;
+        }
+        else 
+        {
+            return 1;
+        }
+    }
+
     private void checkJoystick()
     {
         if(trackpad != null)
         {
-            camZMovement = checkHold(trackpad.GetAxis(Right).x);
-            camXMovement = checkHold(trackpad.GetAxis(Right).y);
-            camYMovement = checkHold(trackpad.GetAxis(Left).y);
+                            print("checking");
+
+            if(SteamVR.instance.hmd_ModelNumber == "VIVE_Pro MV")
+            {
+                if(boolean.GetState(Left))
+                {
+                    camYMovement = checkHold(trackpad.GetAxis(Left).y);
+                }
+                if(boolean.GetState(Right))
+                {
+                    camXMovement = checkHold(trackpad.GetAxis(Right).x);
+                    camZMovement = checkHold(trackpad.GetAxis(Right).y);
+                }
+            }
+            else
+            {
+                camYMovement = checkHold(trackpad.GetAxis(Left).y);
+                camXMovement = -1 * checkHold(trackpad.GetAxis(Right).x);
+                camZMovement = checkHold(trackpad.GetAxis(Right).y);
+            }
+
         }
+    }
+
+    private void checkKeyboard()
+    {
+        camXMovement = checkHold(KeyCode.D, KeyCode.A);
+        camYMovement = checkHold(KeyCode.S, KeyCode.W);
+        camZMovement = checkHold(KeyCode.G, KeyCode.T);
     }
 
     private void updateX()
