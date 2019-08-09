17a76009c2f0f84724177b7eca922f58c40f4a6b
Cyrus Diego <cdiego@ualberta.ca>
2019-08-09 13:23:13 -0600

    limit for wrist (rot and ext) not setting correctly need to check output from brachioplexus also need to have start position all the way to the end of joitn limits

--- VR-Bento-Arm/Assets/Scripts/GameTask/StartPosition.cs
+++ VR-Bento-Arm/Assets/Scripts/GameTask/StartPosition.cs
@@ -8,11 +8,16 @@ public class StartPosition : MonoBehaviour
     public GameObject elbow = null;
     private bool shoulderReady;
     private bool elbowReady;
+    private float shoulderAngle;
+    private float elbowAngle;
     void Awake()
     {
         global.startup = true;
         shoulderReady = false;
         elbowReady = false;
+
+        getLimit();
+
         global.brachIOplexusControl[0] = new Tuple<float, float>(2,0.7f);
         global.brachIOplexusControl[1] = new Tuple<float, float>(1,0.7f);
         global.brachIOplexusControl[2] = new Tuple<float, float>(0,0);
@@ -20,14 +25,34 @@ public class StartPosition : MonoBehaviour
         global.brachIOplexusControl[4] = new Tuple<float, float>(0,0);
     }
 
+    private void getLimit()
+    {
+        byte lowPMin;
+        byte lowPMax;
+        byte hiPMin;
+        byte hiPMax;
+        ushort PMin;
+        ushort PMax;
+
+        lowPMin = global.jointLimits[(4 * arrayIndex) + 0];
+        hiPMin = global.jointLimits[(4 * arrayIndex) + 1];
+        lowPMax = global.jointLimits[(4 * arrayIndex) + 2];
+        hiPMax = global.jointLimits[(4 * arrayIndex) + 3];
+
+        PMin = (ushort)((lowPMin) | (hiPMin << 8));
+        PMax = (ushort)((lowPMax) | (hiPMax << 8));
+    }
+
     void FixedUpdate()
     {
-        if(shoulder.GetComponent<Transform>().rotation.eulerAngles.y <= 325 && shoulder.GetComponent<Transform>().rotation.eulerAngles.y >= 324) 
+        shoulderAngle = shoulder.GetComponent<Transform>().rotation.eulerAngles.y;
+        elbowAngle = elbow.GetComponent<Transform>().rotation.eulerAngles.x;
+        if(shoulderAngle <= 325 && shoulderAngle >= 324) 
         {
             global.brachIOplexusControl[0] = new Tuple<float, float>(0,0);
             shoulderReady = true;
         }
-        if(elbow.GetComponent<Transform>().rotation.eulerAngles.x <= 30 && elbow.GetComponent<Transform>().rotation.eulerAngles.x >= 29) 
+        if(elbowAngle <= 30 && elbowAngle >= 29) 
         {
             global.brachIOplexusControl[1] = new Tuple<float, float>(0,0);
             elbowReady = true;
@@ -37,8 +62,6 @@ public class StartPosition : MonoBehaviour
         {
             global.startup = false;
         }
-
-
     }
 
 }
--- VR-Bento-Arm/Assets/Scripts/Motor.cs
+++ VR-Bento-Arm/Assets/Scripts/Motor.cs
@@ -19,9 +19,11 @@ public class Motor : RotationBase
 
     void Start()
     {
-        Tuple<ushort,ushort> limits; 
+        Tuple<int, int> limits; 
 
         axis = ((Axis)Enum.Parse(typeof(Axis),rotationAxis));
+        min = new SoftJointLimit();
+        max = new SoftJointLimit();
 
         cj = gameObject.GetComponent<ConfigurableJoint>();
         rb = gameObject.GetComponent<Rigidbody>();
@@ -29,17 +31,7 @@ public class Motor : RotationBase
 
         configureCJ();
         configureRB();
-        // limits = configureJointLimits();
-
-        // min.limit = limits.Item1;
-        // max.limit = limits.Item2;
-
-        // cj.lowAngularXLimit = min;
-        // cj.highAngularXLimit = max;
-
-        // SoftJointLimit minP = new SoftJointLimit();
-        // minP.limit = 10;
-        // cj.lowAngularXLimit = minP;
+        configureJointLimits();
     }
 
     void FixedUpdate()
@@ -87,7 +79,7 @@ public class Motor : RotationBase
         cj.yMotion = ConfigurableJointMotion.Locked;
         cj.zMotion = ConfigurableJointMotion.Locked;
 
-        cj.angularXMotion = ConfigurableJointMotion.Free;
+        cj.angularXMotion = ConfigurableJointMotion.Limited;
         cj.angularYMotion = ConfigurableJointMotion.Locked;
         cj.angularZMotion = ConfigurableJointMotion.Locked;
 
@@ -127,7 +119,7 @@ public class Motor : RotationBase
         rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
     }
 
-    private Tuple<ushort,ushort> configureJointLimits()
+    private void configureJointLimits()
     {
         byte lowPMin;
         byte lowPMax;
@@ -135,7 +127,6 @@ public class Motor : RotationBase
         byte hiPMax;
         ushort PMin;
         ushort PMax;
-        Tuple<ushort,ushort> limits;
 
         lowPMin = global.jointLimits[(4 * arrayIndex) + 0];
         hiPMin = global.jointLimits[(4 * arrayIndex) + 1];
@@ -145,9 +136,17 @@ public class Motor : RotationBase
         PMin = (ushort)((lowPMin) | (hiPMin << 8));
         PMax = (ushort)((lowPMax) | (hiPMax << 8));
 
-        limits = new Tuple<ushort, ushort>(10, 190);
+        int actualPMin;
+        int actualPMax;
 
-        return limits;
+        actualPMin = (180 - PMin) - 90;
+        actualPMax = 90 - (PMax - 180);
+
+        min.limit = actualPMin;
+        max.limit = actualPMax;
+
+        cj.lowAngularXLimit = min;
+        cj.highAngularXLimit = max;
     }
 }
 
