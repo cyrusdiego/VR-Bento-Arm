fcaad97570e73e97a3bd35fa3e576f63d57e6aac
Cyrus Diego <cdiego@ualberta.ca>
2019-08-20 12:20:42 -0600

    deleted unused motor scripts

--- VR-Bento-Arm/Assets/Scripts/Tracker/ArmTracker.cs
+++ VR-Bento-Arm/Assets/Scripts/Tracker/ArmTracker.cs
@@ -10,7 +10,7 @@ public class ArmTracker : MonoBehaviour
     const float yaw = 90;
     const float pitch = 0;
 
-    public GameObject capsule = null;
+    public GameObject bentoArm = null;
     public SteamVR_TrackedObject Tracker;
 
     void Update () 
@@ -18,18 +18,14 @@ public class ArmTracker : MonoBehaviour
         //Collect delta rotation and displacement between Tracker and Accessory
         Vector3 delta_displacement = new Vector3(dX, dY, dZ);
         Quaternion delta_rotation = Quaternion.Euler(roll, yaw, pitch);
-        // print("printing delta displacement");
-        // print("dx: " + delta_displacement.x + " dy: " + delta_displacement.y + " dz: " + delta_displacement.z);
-        // print("printing delta rotation");
-        // print(delta_rotation);
-        // print(delta_rotation.w);
+
         //Get current Tracker pose
         Vector3 tracker_position = Tracker.transform.position;
         Quaternion tracker_rotation = Tracker.transform.rotation;
+
         //Transform current Tracker pose to Accessory pose
-        capsule.GetComponent<Transform>().position = tracker_position + (tracker_rotation *
-        delta_rotation) * delta_displacement;
-        capsule.GetComponent<Transform>().rotation = tracker_rotation * delta_rotation;
+        bentoArm.GetComponent<Transform>().position = tracker_position + (tracker_rotation * delta_rotation) * delta_displacement;
+        bentoArm.GetComponent<Transform>().rotation = tracker_rotation * delta_rotation;
 
     }
 }
