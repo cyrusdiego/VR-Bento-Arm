3087fd251b503ea0b2250e0e5f570547ad35eca2
Cyrus Diego <cdiego@ualberta.ca>
2019-08-22 12:48:04 -0600

    creating another bentoarm branch to try to configure torques

--- VR-Bento-Arm/Assets/Scripts/test.cs
+++ VR-Bento-Arm/Assets/Scripts/test.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     Rigidbody rb = null;
+    ConfigurableJoint cj = null;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        cj = gameObject.GetComponent<ConfigurableJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(cj.currentTorque);
     }
 
-    void OnCollisionEnter(Collision other)
-    {
-        Vector3 impulse; 
-        impulse = other.impulse;
+    // void OnCollisionEnter(Collision other)
+    // {
+    //     Vector3 impulse; 
+    //     impulse = other.impulse;
 
-        Vector3 force;
-        force = impulse / Time.fixedDeltaTime;
+    //     Vector3 force;
+    //     force = impulse / Time.fixedDeltaTime;
 
-        print(force);
-    }
+    //     print(force);
+    // }
 }
